c75170ad209a461eca07f1e07d0350e869dd9454
Vishu Garg <91338913+Vishu-12@users.noreply.github.com>
2022-06-16 19:00:27 +0530

    Add files

---
+++ quiz.js
@@ -0,0 +1,93 @@
+
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function () {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function (answer) {
+    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function () {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+}
+
+
+function populate() {
+    if (quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        var choices = quiz.getQuestionIndex().choices;
+        for (var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function () {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+var questions = [
+    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML", "CSS", "HTML"], "HTML"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery", "Django", "NodeJS"], "Django"),
+    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
+    new Question("How many sizes of headers are available in HTML by default?", ["2", "4", "5", "6"], "6"),
+    new Question("HTML files are saved by default with the extension?", [".h", ".html", ".ht", "none"], ".html"),
+    new Question("Which of the following CSS framework is used to create a responsive design?", ["django", "rails", "larawell", "bootstrap"], "bootstrap"),
+    new Question("Which of the following CSS style property is used to specify an italic text?", ["style", "font", "font-style", "@font-style"], "font-style"),
+    new Question("Which of the following is not the property of the CSS box model?", ["margin", "color", "width", "height"], "color"),
+
+];
+
+
+var quiz = new Quiz(questions);
+populate();
+startCount();
